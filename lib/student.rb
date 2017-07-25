6630406e1a0fb71a291d6d0b897cb958df81a866
kitet <kitetnet@gmail.com>
2017-07-25 11:25:43 +0300

    edited to add relation to the through associations

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::
     has_many :studentassignments
+    has_many :associations
     has_many :assignments, through :studentassignments
+    has_many :parents, through :associations
 end
