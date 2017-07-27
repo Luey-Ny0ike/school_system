a05ffd7c0d6922b57db4d499d6557da1214f9575
lewis nyoike <nyoikelewis@gmail.com>
2017-07-27 10:50:55 +0300

    Added a comment to explain the scope

--- lib/assignment.rb
+++ lib/assignment.rb
@@ -1,6 +1,6 @@
 class Assignment < ActiveRecord::Base
     has_many :tracks
     has_many :students, through: :tracks
-
+    #return assignments assigned to this student
     scope :student_assignment,->(level,stream) {where(level: level, stream: stream)}
 end
