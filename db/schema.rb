0b67e05dfff4bd91af5012f3000ea136a5d03565
lewis nyoike <nyoikelewis@gmail.com>
2017-07-27 10:51:22 +0300

    new migrations created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726113546) do
+ActiveRecord::Schema.define(version: 20170726150834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20170726113546) do
     t.string "email"
     t.string "username"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "perfomances", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "grade_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
