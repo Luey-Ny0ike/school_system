16c5df1ee5160f3ad4d0b31521549e0d18bbc3f8
lewis nyoike <nyoikelewis@gmail.com>
2017-07-27 12:05:56 +0300

    Included a scope to return a student grade

--- lib/perfomances.rb
+++ lib/perfomances.rb
@@ -1,4 +1,5 @@
 class Perfomances < ActiveRecord::Base
   belongs_to :students
   belongs_to :grades
+  scope :student_grade,->(student_id,grade_id) {where(student_id: student_id, grade_id: grade_id)}
 end
