a6db01d7c12d7d738117e1282ae579074123d3a3
lewis nyoike <nyoikelewis@gmail.com>
2017-07-27 10:50:21 +0300

    Added the student and grade association through performances

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,5 @@ class Student < ActiveRecord::Base
     has_many :associations
     has_many :assignments, through: :tracks
     has_many :parents, through: :associations
+    has_many :grades, through: :perfomances
 end
