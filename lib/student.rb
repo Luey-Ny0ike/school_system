4d1e62f53b2b086811bcae8f86962d4a3a5f0207
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-27 22:23:38 +0300

    this commit modifies the relationship between student and fees to one to one

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,5 @@ class Student < ActiveRecord::Base
   has_many :associations
   has_many :assignments, through: :tracks
   has_many :parents, through: :associations
-  has_many :fees
+  has_one :fees
 end
