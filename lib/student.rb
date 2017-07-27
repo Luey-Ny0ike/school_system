97bb26e38d1bf3e909bded18485d1ae781854637
kitet <kitetnet@gmail.com>
2017-07-27 10:19:43 +0300

    edited association from studentassignments to tracks

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
-    has_many :tracks
-    has_many :associations
-    has_many :assignments, through: :tracks
-    has_many :parents, through: :associations
+  has_many :tracks
+  has_many :associations
+  has_many :assignments, through: :tracks
+  has_many :parents, through: :associations
 end
